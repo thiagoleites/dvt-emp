385ebff531773bb2b0ff012e70c763d785b82cef
Thiago Leite <thiagoleites@gmail.com>
2026-02-23 00:02:55 -0300

    feat: Wrap NuxtPage with NuxtLayout and set dev server port to 3001.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  devServer: {
+    port: 3001
+  },
   css: ['~/assets/css/main.css'],
   modules: [
     '@vueuse/nuxt',
